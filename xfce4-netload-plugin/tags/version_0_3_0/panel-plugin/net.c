9c12d7d4cba89485a73b06c46820203032cebffb
(no author) <(no author)@6bd62a53-d0f8-0310-92b8-b27fb566919e>
2005-02-04 18:13:04 +0000

    This commit was manufactured by cvs2svn to create tag 'version_0_3_0'.

---
+++ xfce4-netload-plugin/tags/version_0_3_0/panel-plugin/net.c
@@ -0,0 +1,218 @@
+/*
+ * Id: $Id: net.c,v 1.7 2005/02/04 18:12:01 bwalle Exp $
+ * -------------------------------------------------------------------------------------------------
+ * 
+ * This program is free software; you can redistribute it and/or modify it under the terms of the 
+ * GNU General Public License as published by the Free Software Foundation; You may only use 
+ * version 2 of the License, you have no option to use any other version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See 
+ * the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if 
+ * not, write to the Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * ------------------------------------------------------------------------------------------------- 
+ */
+
+/*
+ * This is just a wrapper between the netload-plugin and the wormulon source.
+ * Wormulon is a small command-line util which displays the netload. You can find it
+ * at http://raisdorf.net/wormulon. Most sourcecode is taken from wormulon.
+ *
+ * Thanks to Hendrik Scholz. Only his work made it possible to support a large
+ * number of operating systems quickly without a library! Without him only 
+ * Linux and FreeBSD (with foreign code from IceWM) would be supported.
+ */
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+/* From Wormulon */
+#include "net.h"
+#include "os.h"
+#include "wormulon.h"
+#include "slurm.h"	/* slurm structs */
+
+#include <sys/types.h>
+
+
+#ifdef __HPUX__
+# include "wormulon/hpux.h"
+# include "wormulon/hpux.c"
+#elif __APPLE__
+# include "src/macos.h"
+# include "src/macos.c"
+#elif __FreeBSD__
+# include "wormulon/freebsd.h"
+# include "wormulon/freebsd.c"
+#elif __linux__
+# include "wormulon/linux.h"
+# include "wormulon/linux.c"
+#elif __OpenBSD__ || __MicroBSD__
+# include "wormulon/openbsd.h"
+# include "wormulon/openbsd.c"
+#elif __NetBSD__
+# include "wormulon/netbsd.h"
+# include "wormulon/netbsd.c"
+#elif __Solaris__
+# include "wormulon/solaris.h"
+# include "wormulon/solaris.c"
+#else
+/* should not get here */
+# error "OS not supported"
+#endif
+
+
+/* ---------------------------------------------------------------------------------------------- */
+int init_netload(netdata* data, const char* device)
+{
+    memset( data, 0, sizeof(netdata) );
+    
+    if (device == NULL || strlen(device) == 0)
+    {
+        return TRUE;
+    }
+    
+    strncpy( data->ifdata.if_name, device, INTERFACE_NAME_LENGTH);
+    data->ifdata.if_name[INTERFACE_NAME_LENGTH] = '\0';
+    
+    init_osspecific( data );
+    
+    data->ip_address[0] = 0;
+    data->ip_update_count = 0;
+    
+    if (checkinterface(data) != TRUE)
+	{
+        data->correct_interface = FALSE;
+		return FALSE;
+	}
+    
+    /* init in a sane state */
+    get_stat(data);
+    data->backup_in  = data->stats.rx_bytes;
+    data->backup_out = data->stats.tx_bytes;
+    
+    data->correct_interface = TRUE;
+    
+#ifdef DEBUG
+    fprintf( stderr, "The netload plugin was initialized for '%s'.\n", device );
+#endif /* DEBUG */
+    
+    return TRUE;
+}
+
+
+/* ---------------------------------------------------------------------------------------------- */
+void get_current_netload(netdata* data, unsigned long *in, unsigned long *out, unsigned long *tot) 
+{
+    struct timeval curr_time;
+    double delta_t;
+    
+    if (! data->correct_interface)
+    {
+        if (in != NULL && out != NULL && tot != NULL)
+        {
+            *in = *out = *tot = 0;
+        }
+    }
+    
+    gettimeofday(&curr_time, NULL);
+    
+    delta_t = (double) ((curr_time.tv_sec  - data->prev_time.tv_sec) * 1000000L
+                             + (curr_time.tv_usec - data->prev_time.tv_usec)) / 1000000.0;
+    
+    /* update */
+    get_stat(data);
+    if (data->backup_in > data->stats.rx_bytes)
+    {
+        data->cur_in = (int)( data->stats.rx_bytes / delta_t + 0.5);
+    }
+    else
+    {
+        data->cur_in = (int)( (data->stats.rx_bytes - data->backup_in) / delta_t + 0.5);
+    }
+	
+    if (data->backup_out > data->stats.tx_bytes)
+    {
+        data->cur_out = (int)( data->stats.tx_bytes / delta_t + 0.5);
+    }
+    else
+    {
+        data->cur_out = (int)( (data->stats.tx_bytes - data->backup_out) / delta_t + 0.5);
+    }
+
+    if (in != NULL && out != NULL && tot != NULL)
+    {
+        *in = data->cur_in;
+        *out = data->cur_out;
+        *tot = *in + *out;
+    }
+    
+    /* save 'new old' values */
+    data->backup_in = data->stats.rx_bytes;
+    data->backup_out = data->stats.tx_bytes;
+    
+    /* do the same with time */
+    data->prev_time.tv_sec = curr_time.tv_sec;
+    data->prev_time.tv_usec = curr_time.tv_usec;
+}
+
+
+/* ---------------------------------------------------------------------------------------------- */
+char* get_name(netdata* data)
+{
+    return data->ifdata.if_name;
+}
+
+
+/* ---------------------------------------------------------------------------------------------- */
+char* get_ip_address(netdata* data)
+{
+    int sockfd;
+    struct ifreq ifr;
+    struct sockaddr_in *p_sa;
+        
+    /* use cached value if possible and if the update count is non-zero */ 
+    if (data->ip_address && data->ip_update_count > 0)
+    {
+        data->ip_update_count--;
+        return data->ip_address;
+    }
+    
+    /* get the value from the operating system */
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+    {
+        perror("Error in socket");
+        return NULL;
+    }
+    
+    snprintf(ifr.ifr_name, IF_NAMESIZE, data->ifdata.if_name);
+    if (ioctl(sockfd, SIOCGIFADDR, &ifr) != 0)
+    {
+        perror("Error in ictl(sockfd)");
+        return NULL;
+    }
+    
+    p_sa = (struct sockaddr_in*) &ifr.ifr_addr;
+    
+    if (!inet_ntop(AF_INET, &p_sa->sin_addr, data->ip_address, IP_ADDRESS_LENGTH))
+    {
+        perror("Error in inet_ntop");
+        return NULL;
+    }
+    
+    /* now updated */
+    data->ip_update_count = IP_UPDATE_INTERVAL;
+    
+    return data->ip_address;
+}
+
+
+/* ---------------------------------------------------------------------------------------------- */
+void close_netload(netdata* data)
+{
+    /* We need not code here */
+}
+
---
+++ xfce4-netload-plugin/tags/version_0_3_0/panel-plugin/net.h
@@ -0,0 +1,129 @@
+/*
+ * Id: $Id: net.h,v 1.8 2005/02/04 18:12:01 bwalle Exp $
+ * -------------------------------------------------------------------------------------------------
+ * 
+ * This program is free software; you can redistribute it and/or modify it under the terms of the 
+ * GNU General Public License as published by the Free Software Foundation; You may only use 
+ * version 2 of the License, you have no option to use any other version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See 
+ * the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if 
+ * not, write to the Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * ------------------------------------------------------------------------------------------------- 
+ */
+#ifndef NET_H
+#define NET_H
+
+#include "os.h"
+#include "slurm.h"
+
+#define MSGSIZE 1024
+#define IP_UPDATE_INTERVAL 20
+#define IP_ADDRESS_LENGTH 64
+#define INTERFACE_NAME_LENGTH 9
+
+#ifndef gettext_noop
+#define gettext_noop(String) String
+#endif
+
+/** errorcodes */
+typedef enum 
+{
+    UNKNOWN_ERROR,
+    PROC_DEVICE_NOT_FOUND,
+    INTERFACE_NOT_FOUND
+} errorcode_t;
+
+
+/**
+ * We need this because we cannot use static variables. Using of static variables allows
+ * us not to use several instances of the plugin.
+ * I know that this change makes it a bit incompatible with wormulon, but that's the
+ * price to pay ...
+ */
+typedef struct
+{
+    char            old_interface[INTERFACE_NAME_LENGTH];
+    double          backup_in;
+    errorcode_t     errorcode;
+    double          backup_out;
+    double          cur_in;
+    double          cur_out;
+    struct timeval  prev_time;
+    int             correct_interface;          /* treated as boolean */
+    IfData          ifdata;
+    char            ip_address[IP_ADDRESS_LENGTH];
+    int             ip_update_count;
+    DataStats       stats;
+#ifdef __HPUX__
+    int             wait_pcks_counter;
+    nmapi_logstat*  if_ptr;
+#elif __FreeBSD__
+    int             watchif;
+    int             dev_opened;
+#elif __NetBSD__
+    int             mib_name1[6];
+    int             mib_name2[6];
+    char*           buf1;
+    char*           buf2;
+    int             alloc1;
+    int             alloc2;
+#elif __OpenBSD__ || __MicroBSD__ || __APPLE__
+    int             mib_name1[6];
+    int             mib_name2[6];
+    char*           buf1;
+    char*           buf2;
+    int             alloc1;
+    int             alloc2;
+#elif __linux__
+    FILE*           proc_net_dev;
+#elif __Solaris__
+#else
+#error "OS not supported"
+#endif
+    
+} netdata;
+
+
+/**
+ * Initializes the netload plugin. Used to set up inital values. This function must
+ * be called after each change of the network interface.
+ * @param   device      The network device, e.g. <code>ippp0</code> for ISDN on Linux.
+ * @return  <code>true</code> if no error occurs, <code>false</code> otherwise. If there's
+ *          an error, the error message may be set
+ */
+int init_netload(netdata* data, const char* device);
+
+/**
+ * Gets the current netload. You must call init_netload() once before you use this function!
+ * @param in        Input load in byte/s.
+ * @param out       Output load in byte/s.
+ * @param tot       Total load in byte/s.
+ */
+void get_current_netload(netdata* data, unsigned long *in, unsigned long *out, unsigned long *tot);
+
+/**
+ * Returns the name of the network interface.
+ * @param data      object
+ * @return The name. String resides in data and you don't have to free the string.
+ *         On error, returns NULL.
+ */
+char* get_name(netdata* data);
+
+/**
+ * Returns the IP address of the network interface
+ * @param data     object
+ * @return the IP address as string, NULL on error.
+ */
+char* get_ip_address(netdata* data);
+
+/**
+ * Should be called to do cleanup work.
+ */
+void close_netload(netdata* data);
+
+#endif /* NET_H */
