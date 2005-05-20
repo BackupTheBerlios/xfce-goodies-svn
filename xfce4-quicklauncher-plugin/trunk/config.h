3145f8913d5e206e353feae2c8fd0c1efff525b0
huysmans <huysmans@6bd62a53-d0f8-0310-92b8-b27fb566919e>
2005-05-20 15:43:27 +0000

    Fix (hopefully) gettext stuff

--- xfce4-quicklauncher-plugin/trunk/config.h
+++ xfce4-quicklauncher-plugin/trunk/config.h
@@ -7,11 +7,32 @@
 /* Define for tracing support */
 /* #undef DEBUG_TRACE */
 
+/* always defined to indicate that i18n is enabled */
+#define ENABLE_NLS 1
+
+/* Name of default gettext domain */
+#define GETTEXT_PACKAGE "xfce4-quicklauncher-plugin"
+
+/* Define to 1 if you have the `bind_textdomain_codeset' function. */
+#define HAVE_BIND_TEXTDOMAIN_CODESET 1
+
+/* Define to 1 if you have the `dcgettext' function. */
+#define HAVE_DCGETTEXT 1
+
 /* Define to 1 if you have the <dlfcn.h> header file. */
 #define HAVE_DLFCN_H 1
 
+/* Define if the GNU gettext() function is already present or preinstalled. */
+#define HAVE_GETTEXT 1
+
 /* Define to 1 if you have the <inttypes.h> header file. */
 #define HAVE_INTTYPES_H 1
+
+/* Define if your <locale.h> file defines LC_MESSAGES. */
+#define HAVE_LC_MESSAGES 1
+
+/* Define to 1 if you have the <locale.h> header file. */
+#define HAVE_LOCALE_H 1
 
 /* Define to 1 if you have the <memory.h> header file. */
 #define HAVE_MEMORY_H 1
@@ -38,7 +59,7 @@
 #define HAVE_UNISTD_H 1
 
 /* Name of package */
-#define PACKAGE "xfce4-Quicklauncher-plugin"
+#define PACKAGE "xfce4-quicklauncher-plugin"
 
 /* Define to the address where bug reports for this package should be sent. */
 #define PACKAGE_BUGREPORT ""
@@ -59,7 +80,7 @@
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "0.5.0"
+#define VERSION "0.8.1"
 
 /* Define to 1 if on AIX 3.
    System headers sometimes define this.
