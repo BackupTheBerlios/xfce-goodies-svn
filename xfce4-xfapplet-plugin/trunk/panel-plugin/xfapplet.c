ebce3bf477c68327f6069e317ac76a5e7a45e143
adrianob <adrianob@6bd62a53-d0f8-0310-92b8-b27fb566919e>
2006-02-01 01:09:05 +0000

    Read and save the plugin's configuration.

--- xfce4-xfapplet-plugin/trunk/panel-plugin/xfapplet.c
+++ xfce4-xfapplet-plugin/trunk/panel-plugin/xfapplet.c
@@ -22,6 +22,7 @@
 
 #include <gtk/gtk.h>
 #include <libbonoboui.h>
+#include <libxfce4util/libxfce4util.h>
 #include <libxfce4panel/xfce-panel-plugin.h>
 
 #define MOVE_MENU_ITEM_ORDER            4
@@ -32,9 +33,8 @@
 typedef struct  {
 
 	XfcePanelPlugin   *plugin;
+	gchar             *moniker;
 	GtkWidget         *combo;
-	GtkWidget         *eb;
-	gulong             configure_signal;
 	
 } XfAppletPlugin;
 
@@ -106,22 +106,10 @@ xfapplet_applet_activated (BonoboWidget *bw, CORBA_Environment *ev, gpointer dat
 }
 
 static void
-xfapplet_load_applet (XfAppletPlugin *xap)
-{
-	gchar *applet;
-
-	g_signal_handler_disconnect (xap->plugin, xap->configure_signal);
-	gtk_widget_destroy (xap->eb);
-
-	applet = gtk_combo_box_get_active_text (GTK_COMBO_BOX(xap->combo));
-	bonobo_widget_new_control_async (applet, CORBA_OBJECT_NIL, xfapplet_applet_activated, xap);
-
-	g_free (applet);
-}
-
-static void
 xfapplet_free(XfcePanelPlugin *plugin, XfAppletPlugin *xap)
 {
+	if (xap->moniker)
+		g_free (xap->moniker);
 	g_free (xap);
 }
 
@@ -150,10 +138,13 @@ xfapplet_create_applet_list(XfAppletPlugin *xap)
 	CORBA_free (applets);
 }
 
+static gboolean xfapplet_setup_full (XfAppletPlugin*);
+
 static void
 xfapplet_options_response (GtkWidget *dialog, int response, XfAppletPlugin *xap)
 {
-	xfapplet_load_applet (xap);
+	xap->moniker = gtk_combo_box_get_active_text (GTK_COMBO_BOX(xap->combo));
+	xfapplet_setup_full (xap);
 	gtk_widget_destroy (dialog);
 	xfce_panel_plugin_unblock_menu (xap->plugin);
 }
@@ -187,25 +178,108 @@ xfapplet_options(XfcePanelPlugin *plugin, XfAppletPlugin *xap)
 	gtk_widget_show_all (dialog);
 }
 
-static XfAppletPlugin*
-xfapplet_new (XfcePanelPlugin *plugin)
+static gboolean
+xfapplet_setup_full (XfAppletPlugin *xap)
 {
-	XfAppletPlugin *xap;
+	GList     *list;
+
+	list = gtk_container_get_children (GTK_CONTAINER (xap->plugin));
+	if (list && list->data)
+		gtk_widget_destroy (GTK_WIDGET (list->data));
+
+	bonobo_widget_new_control_async (xap->moniker, CORBA_OBJECT_NIL, xfapplet_applet_activated, xap);
+}
+
+static gboolean
+xfapplet_setup_empty (XfAppletPlugin *xap)
+{
 	GtkWidget *ask, *eb;
-
-	xap = g_new0 (XfAppletPlugin, 1);
-	xap->plugin = plugin;
-
+	
 	ask = gtk_label_new (" ?? ");
 	gtk_widget_show (ask);
 
 	eb = gtk_event_box_new ();
-	xap->eb = eb;
 	gtk_container_add (GTK_CONTAINER (eb), ask);
 	gtk_widget_show (eb);
 	
-	gtk_container_add (GTK_CONTAINER(plugin), eb);
-	xfce_panel_plugin_add_action_widget (plugin, eb);
+	gtk_container_add (GTK_CONTAINER(xap->plugin), eb);
+	xfce_panel_plugin_add_action_widget (xap->plugin, eb);
+	xfce_panel_plugin_menu_show_configure (xap->plugin);
+
+	g_signal_connect (xap->plugin, "configure-plugin", G_CALLBACK (xfapplet_options), xap);
+}
+
+static void
+xfapplet_save_configuration (XfcePanelPlugin *plugin, gpointer data)
+{
+	XfceRc         *config;
+	gchar          *path;
+	XfAppletPlugin *xap = data;
+
+	if (!xap->moniker)
+		return;
+
+	path = xfce_panel_plugin_lookup_rc_file (plugin);
+	if (!path)
+		path = xfce_panel_plugin_save_location (plugin, TRUE);
+
+	if (!path)
+		return;
+
+        config = xfce_rc_simple_open (path, FALSE);
+	g_free (path);
+
+	if (!config)
+		return;
+
+	xfce_rc_set_group (config, "xfapplet");
+
+	/* Moniker for bonobo control */
+	xfce_rc_write_entry (config, "moniker", xap->moniker);
+
+	xfce_rc_close (config);
+}
+
+static gboolean
+xfapplet_read_configuration (XfAppletPlugin *xap)
+{
+	XfceRc       *config;
+	gchar        *path;
+	const gchar  *moniker;
+
+	path = xfce_panel_plugin_lookup_rc_file (xap->plugin);
+	if (!path)
+		return FALSE;
+
+	config = xfce_rc_simple_open (path, TRUE);
+	g_free (path);
+
+	if (!config)
+		return FALSE;
+
+	xfce_rc_set_group (config, "xfapplet");
+
+	/* Moniker for bonobo control */
+	moniker = xfce_rc_read_entry (config, "moniker", NULL);
+	if (!moniker) {
+		xfce_rc_close (config);
+		return FALSE;
+	}
+	xap->moniker = g_strdup (moniker);
+
+	xfce_rc_close (config);
+
+	return TRUE;
+}
+
+static XfAppletPlugin*
+xfapplet_new (XfcePanelPlugin *plugin)
+{
+	XfAppletPlugin *xap;
+
+	xap = g_new0 (XfAppletPlugin, 1);
+	xap->plugin = plugin;
+	xap->moniker = NULL;
 
 	return xap;
 }
@@ -222,11 +296,13 @@ xfapplet_construct (XfcePanelPlugin *plugin)
 	if (!(xap = xfapplet_new (plugin)))
 		exit (1);
 
-	xfce_panel_plugin_menu_show_configure (plugin);
+	if (xfapplet_read_configuration (xap))
+		xfapplet_setup_full (xap);
+	else
+		xfapplet_setup_empty (xap);
 
-	xap->configure_signal =
-		g_signal_connect (plugin, "configure-plugin", G_CALLBACK (xfapplet_options), xap);
 	g_signal_connect (plugin, "free-data", G_CALLBACK (xfapplet_free), xap);
+	g_signal_connect (plugin, "save", G_CALLBACK (xfapplet_save_configuration), xap);
 }
 
 XFCE_PANEL_PLUGIN_REGISTER_EXTERNAL(xfapplet_construct)
