d5c59aef740f9e9d2587450edecf8eb7dbfb3481
sasoiliev <sasoiliev@6bd62a53-d0f8-0310-92b8-b27fb566919e>
2006-03-16 23:33:08 +0000

    Tagging version 0.3.3 before commiting changes for Xfce 4.4 panel interface.

---
+++ xfce4-xkb-plugin/tags/SASOILIEV-V0.3.3-PRE_XFCE4_4/panel-plugin/xkb.h
@@ -0,0 +1,70 @@
+/*
+//====================================================================
+//  xfce4-xkb-plugin - XFCE4 Xkb Layout Indicator panel plugin
+// -------------------------------------------------------------------
+//  Alexander Iliev <sasoiliev@mail.bg>
+//  20-Feb-04
+// -------------------------------------------------------------------
+//  Parts of this code belong to Michael Glickman <wmalms@yahooo.com>
+//  and his program wmxkb.
+//  WARNING: DO NOT BOTHER Michael Glickman WITH QUESTIONS ABOUT THIS
+//           PROGRAM!!! SEND INSTEAD EMAILS TO <sasoiliev@mail.bg>
+//====================================================================
+*/
+
+#ifndef _XFCE_XKB_H_
+#define _XFCE_XKB_H_
+
+#include <X11/Xlib.h>
+#include <gtk/gtk.h>
+#include <glib.h>
+
+/* # define TINY 0
+# define SMALL 1
+# define MEDIUM 2
+# define LARGE 3
+*/
+#define ICONSIZETINY 24 
+#define ICONSIZESMALL 30
+#define ICONSIZEMEDIUM 45
+#define ICONSIZELARGE 60
+
+typedef enum {
+  TEXT = 0,
+  IMAGE = 1
+} t_display_type;
+
+typedef struct {
+  GtkWidget	*ebox;
+  GtkWidget *btn;
+  GtkWidget *label;
+  GtkWidget *image;
+  GtkWidget *vbox;
+  GtkWidget *def_lang_menu;
+  
+  gint size;
+  
+  t_display_type display_type;
+
+  // perapps
+  gboolean enable_perapp;
+  // default group for "locale per process"
+  gint default_group;
+} t_xkb;
+
+void set_new_locale(t_xkb *ctrl);
+char *initialize_xkb(t_xkb *ctrl);
+void deinitialize_xkb();
+
+gint get_group_count();
+char * get_symbol_name_by_res_no(int group_res_no);
+
+int do_change_group(int increment, t_xkb *ctrl);
+gboolean gio_callback(GIOChannel *source, GIOCondition condition, gpointer data);
+int get_connection_number();
+
+// "locale per process" functions
+void react_active_window_changed(gint pid, t_xkb *ctrl);
+void react_window_closed(gint pid);
+int do_set_group(gint group, t_xkb *ctrl);
+#endif
