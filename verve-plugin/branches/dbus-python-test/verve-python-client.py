5e7a6443043f2baa62d5026df3e1fe7f33eecf80
jpohlmann <jpohlmann@6bd62a53-d0f8-0310-92b8-b27fb566919e>
2006-03-22 12:16:18 +0000

    Removed experimental D-BUS stuff

--- verve-plugin/branches/dbus-python-test/verve-python-client.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import dbus
-import gtk
-
-bus = dbus.SessionBus()
-remoteObject = bus.get_object("org.xfce.FileManager", "/DBUSService")
-iface = dbus.Interface(remoteObject, "org.xfce.FileManager")
-remoteObject.Launch("exo-open http://xfce.org", 1, dbus_interface = "org.xfce.FileManager")
