a53d7c2a19e6cf74cbdb13f6e39349ab301559bf
bountykiller <bountykiller@6bd62a53-d0f8-0310-92b8-b27fb566919e>
2006-04-25 20:51:35 +0000

    save old revision

--- xfce4-weather-plugin/trunk/panel-plugin/summary_window.h
+++
@@ -1,7 +0,0 @@
-#include "parsers.h"
-#include "get_data.h"
-#include <gtk/gtk.h>
-#include "icon.h"
-
-#define BORDER 6
-GtkWidget *create_summary_window(struct xml_weather *data, enum units unit);
