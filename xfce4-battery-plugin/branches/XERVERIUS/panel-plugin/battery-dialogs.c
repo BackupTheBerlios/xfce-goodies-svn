b2b40e39a2ea93c5b75107a2f38ed592667d1c62
xerverius <xerverius@6bd62a53-d0f8-0310-92b8-b27fb566919e>
2006-05-28 18:53:28 +0000

    * Add destroy with patent

--- xfce4-battery-plugin/branches/XERVERIUS/panel-plugin/battery-dialogs.c
+++ xfce4-battery-plugin/branches/XERVERIUS/panel-plugin/battery-dialogs.c
@@ -372,8 +372,8 @@ battery_configure (XfcePanelPlugin *plugin,
 
 #ifdef USE_NEW_DIALOG
     dialog = xfce_titled_dialog_new_with_buttons (_("Battery Monitor"),
-                                                  NULL,
-                                                  GTK_DIALOG_NO_SEPARATOR,
+                                                  GTK_WINDOW (gtk_widget_get_toplevel (GTK_WIDGET (plugin))),
+                                                  GTK_DIALOG_DESTROY_WITH_PARENT | GTK_DIALOG_NO_SEPARATOR,
                                                   GTK_STOCK_CLOSE, GTK_RESPONSE_OK,
                                                   NULL);
 #else
